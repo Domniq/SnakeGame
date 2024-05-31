b83fab8f6c6015e51a8967d6805401e65d94fb8c
Dominik <dominxw1011@gmail.com>
2024-05-31 07:32:57 +0200

    Added board

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Timers;
+
+char horizontal = '═';
+char vertical = '║';
+char topLeft = '╔';
+char topRight = '╗';
+char bottomLeft = '╚';
+char bottomRight = '╝';
+
+int width = 32;
+int height = 16;
+
+DrawBoard();
+
+void DrawBoard()
+{
+    Console.Clear();
+    Console.WriteLine($"{topLeft}{new string(horizontal, width - 2)}{topRight}");
+
+    for (int i = 0; i < height - 2; i++)
+    {
+        Console.WriteLine($"{vertical}{new string(' ', width - 2)}{vertical}");
+    }
+
+    Console.WriteLine($"{bottomLeft}{new string(horizontal, width - 2)}{bottomRight}");
+}
